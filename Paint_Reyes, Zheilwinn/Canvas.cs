dba7bea626ca96f0c9de92bbcd1c45f309aecb84
BCAS-Student35 <BCAS-Student35@DESKTOP-2AGPVOD>
2022-11-05 13:35:50 +0800

    Add project files.

---
+++ Paint_Reyes, Zheilwinn/Canvas.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Paint_Reyes__Zheilwinn
+{
+    class Paint
+    {
+    }
+}
+
+public class Canvas
---
+++ Paint_Reyes, Zheilwinn/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Paint_Reyes__Zheilwinn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
+public interface IPaintTool
+{
+    void MouseOver();
+    void MouseUp();
+}
+
+public class PencilTool:IPaintTool
+{
+    public void MouseOver()
+    {
+        Console.WriteLine("Show pencil icon");
+    }
+
+    public void MouseUp()
+    {
+        Console.WriteLine("Draw a line.");
+    }
+
+  
+}
+
+public class SquareTool : IPaintTool
+{
+    public void MouseOver()
+    {
+        Console.WriteLine("Show square icon");
+    }
+
+    public void MouseUp()
+    {
+        Console.WriteLine("Draw a square.");
+    }
+}
+
+public class EraserTool : IPaintTool
+{
+    public void MouseOver()
+    {
+        Console.WriteLine("Show an eraser icon");
+    }
+
+    public void MouseUp()
+    {
+        Console.WriteLine("Show an eraser icon");
+    }
+}
+
+
+
+   
+
